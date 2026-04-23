f42af2c767b3228c283fc2f0fc16dadb4d8c9de6
GemmaAgra <gemma_agra@yahoo.es>
2026-04-23 16:03:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,42 +3,49 @@ import numpy as np, utils, random, os
 class JuegoHundirFlota:
 
     def __init__(self):
-        self.tamano = 5
-        self.config_barcos = [2, 2, 3]
+        self.tamano = 10
+        self.config_barcos = [2, 2, 2, 3, 3, 4]
         # Tableros
-        self.tablero_maquina = utils.crear_tablero(self.tamano)
-        self.tablero_jugador = utils.crear_tablero(self.tamano)        
+        self.tablero_maquina = utils.crear_tablero(self.tamano)   # donde pinto los barcos
+        self.tablero_jugador = utils.crear_tablero(self.tamano)   # donde pinto los barcos
         self.radar_jugador   = utils.crear_tablero(self.tamano)   # Visibilidad para el jugador (donde dispara)
         self.radar_rival     = utils.crear_tablero(self.tamano)   # Visibilidad para el rival (donde dispara)
-        self._rival          = utils.crear_tablero(self.tamano)
 
-    def preparar_juego(self):   # coloco los barcos del jugador y maquina
+    def preparar_juego(self):                                     # pinto los barcos del jugador y máquina en el tablero
         self.tablero_maquina = utils.colocar_barcos_aleatorios(self.tablero_maquina, self.config_barcos)
         self.tablero_jugador = utils.colocar_barcos_aleatorios(self.tablero_jugador, self.config_barcos)
-        print("\n Barcos maquina: \n", self.tablero_maquina)
+        print("\n Barcos máquina: \n", self.tablero_maquina)
         print("\n Barcos jugador: \n", self.tablero_jugador)
+
 
     def jugar(self):
         self.preparar_juego()
         print("\n ¡Bienvenido a Hundir la Flota!")
         
-        while "O" in self.tablero_maquina and "O" in self.tablero_jugador:   # O son los barcos
-            
+        while "O" in self.tablero_maquina and "O" in self.tablero_jugador:   # "O"= barcos
+
+            os.system('cls' if os.name == 'nt' else 'clear')       # limpio pantalla
+
+             # pinto SIEMPRE los 2 tableros de los barcos y el de la maquina F5
+            print("\n Barcos máquina: \n", self.tablero_maquina)
+            print("\n Barcos jugador: \n", self.tablero_jugador)
+            print("\n Barcos máquina: \n", self.radar_rival)
+
             # TURNO JUGADOR
-            print("\n--- TU TURNO ---")
-            print("\n", self.radar_jugador)
+            print("\n--- TU TURNO --- \n", self.radar_jugador)         
             try:
                 f = int(input("Fila (0-4): "))
                 c = int(input("Columna (0-4): "))
                 rdo = utils.disparar((f, c), self.tablero_maquina)
                 print(rdo)
-                self.radar_jugador[f, c] = self.tablero_maquina[f, c]
-            except:  # si meto 5, 6...etc
-                print("Coordenada inválida, pierdes el turno")
+                self.radar_jugador[f, c] = self.tablero_maquina[f, c]   # F5 mi tablero para ver el rdo de mi disparo
+            except:  
+                print("Error: Coordenada inválida, pierdes el turno") # controlar error si meto 10, 11.. fuera del tablero
 
-            # TURNO MÁQUINA      VF si la casilla elegida ya ha sido tocada (si ya tiene 'X' o una 'A' de agua)
-            # Antes de q la máquina dispare, VF si le quedan barcos ("O") para evitar q dispare si yo acabo 
-            # de hundir su último barco en el turno ant.
+            # TURNO MÁQUINA      
+            # Antes de q la máquina dispare, VF si a la casilla elegida le quedan barcos 'O'
+            # para evitar q dispare si yo acabo de hundir su último barco en el turno ant.
+
             if "O" in self.tablero_maquina:
                 ataco = False
                 while not ataco:  # La máquina se queda "pensando" hasta q encuentra una coord q no haya disparado antes
@@ -49,17 +56,19 @@ class JuegoHundirFlota:
                     if self.tablero_jugador[f_m, c_m] in ["-", "O"]:
                         utils.disparar((f_m, c_m), self.tablero_jugador)  # Si la máquina acierta, en mi tablero veo una "X" y si falla una "A"
                         print(f"\nLa máquina disparó a ({f_m}, {c_m})")
-                        print("\n--- TURNO MAQUINA ---")
-                        print("\n", self.radar_rival)
+                        print("\n--- TURNO MAQUINA --- \n", self.radar_rival)                        
                         rdo_m = utils.disparar((f_m, c_m), self.tablero_maquina)
                         print(rdo_m)
                         self.radar_rival[f_m, c_m] = self.tablero_maquina[f_m, c_m]
                         ataco = True # sale bucle pq el disparo es VL
 
         if "O" not in self.tablero_maquina:
-            print("¡HAS GANADO!")
+            print("¡HAS GANADO! barco hundido! ")   # si hundes un barco considero q has ganado
         else: print("La máquina ha ganado...")
 
-if __name__ == "__main__":
-    partida = JuegoHundirFlota()
-    partida.jugar()
+if __name__ == "__main__":          # ejecuta el if si el archivo main se abre (ejecuto en terminal "python main.py")
+    partida = JuegoHundirFlota()    # arrancar prog. creando un objeto de mi clase
+    partida.jugar()                 # empiezo el juego
+
+
+# Si importo la clase JuegoHundirFlota desde otro archivo, el juego no se lanzará solo, hay q ejecutarlo con el main.py
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np, random
 
-def crear_tablero(tamano=5):
+def crear_tablero(tamano=10):
     return np.full((tamano, tamano), "-")
 
 def colocar_barco(barco, tablero):
@@ -17,10 +17,11 @@ def disparar(casilla, tablero):
         return "Agua"
     return "Ya disparaste aquí"
 
-def crear_barco(eslora, tamano_tablero=5):    # diseña un barco q exista dentro de los límites del mapa
-    while True:
-        orientacion = random.choice(['H', 'V'])
-        fila = random.randint(0, tamano_tablero - 1)
+
+def crear_barco(eslora, tamano_tablero=10):      # crea un barco q exista dentro del tablero   
+    while True:                                  # bucle infinito de intentos hasta q el barco encuentre un lugar VL 
+        orientacion = random.choice(['H', 'V'])        # genera orient. y coords aleatorias. de 0 a 9 (tamaño 10)
+        fila = random.randint(0, tamano_tablero - 1)  
         col  = random.randint(0, tamano_tablero - 1)
         
         casillas = []
@@ -28,41 +29,44 @@ def crear_barco(eslora, tamano_tablero=5):    # diseña un barco q exista dentro
             if orientacion == 'H': casillas.append((fila, col + i))
             else:                  casillas.append((fila + i, col))
         
-        # VL q no se salga del tablero el barco, volvería a intentarlo hasta q el diseño sea VL
+        # VL q el barco no se salga del tablero, volvería a intentarlo hasta q el diseño sea VL
         if all(0 <= r < tamano_tablero and 0 <= c < tamano_tablero for r, c in casillas):
-            return casillas        
+            return casillas  
         """
-        for r, c in casillas: Recorre cada coord de la lista de casillas del barco. r =fila (row) y c = col.
-        0 <= r < tamano_tablero: Comprueba que la fila no sea negativa y que no sea >= al tamaño del tablero 
-        (si el tablero es de 5, la fila máx es 4).
-        0 <= c < tamano_tablero: Hace lo mismo para la columna.
-        all(...): función de Python q devuelve True solo si todas las condiciones dentro del bucle son true. 
-                  Si una sola casilla se sale (por ej, fila 5 en un tablero de 5), all devuelve False.
+        for r, c in casillas: Recorre cada coord de la lista casillas del barco. r =fila (row) y c = col
+        0 <= r < tamano_tablero: Comprueba q la fila no sea negativa y q sea < al tamaño del tablero 
+        (si el tablero es de 10, la fila máx es 9)
+        0 <= c < tamano_tablero: Hace lo mismo para la col
+        all(...): función de Python q devuelve True si todas las condiciones del bucle son true 
+                  Si 1 casilla se sale (por ej. fila 10 en un tablero de 10), all devuelve False
         """
 
-def colocar_barcos_aleatorios(tablero, barcos_config):   # gestiona toda la flota usando crear_barco, propuesta barco y mira tablero
+
+def colocar_barcos_aleatorios(tablero, barcos_config):   
     tablero_temp = tablero.copy()
-    for eslora in barcos_config:    # Recorre lista de barcos ptes
+    for eslora in barcos_config:    # Recorre lista de barcos a colocar
         colocado = False
         while not colocado:                  
-            propuesta = crear_barco(eslora)          # genera coord aleatorias (VF que el barco no se sale del tablero)        
-            if all(tablero_temp[c] == "-" for c in propuesta):   # VL q no haya otro barco superpuesto. c coord (fila, col)
-                colocar_barco(propuesta, tablero_temp)      # Si hueco libre pongo "O" y sale del while para pasar al
-                colocado = True                             #  sgte barco. Si hay otro barco (O) rechaza la propuesta
-    return tablero_temp                                     # y pide otra
-
-"""
-bucle infinito de intentos. para cuando el barco encuentre un lugar VL
- 
- 
- 
-all(...): Comprueba q todas las casillas q necesita el nuevo barco tengan agua (_), Si 1 tiene una "O" hay colisión 
-y el if falla
+            propuestaCasillas = crear_barco(eslora)     # genera coord aleatorias y VF q el barco no se sale del tablero        
+            if all(tablero_temp[c] == "-" for c in propuestaCasillas): # VL q no haya otro barco superpuesto en c (f-col)
+                colocar_barco(propuestaCasillas, tablero_temp)  # Si hueco libre pongo "O" y sale del while para pasar al
+                colocado = True                     #  sgte barco. Si hay otro barco (O) rechaza la propuesta y pide otra
+    return tablero_temp                                     
+""" 
+all(...): es el "detector de colisiones", VF q todas las casillas q necesita el nuevo barco tengan agua (-)
+Si 1 tiene una "O" hay colisión y el if falla
 
 
-característica	crear_barco	                       colocar_barcos_aleatorios
-Misión	     Crear coordenadas de un barco.     	Gestionar la colocación de toda la flota.
-Validación	Que no se salga del tablero (Bordes).    	Que no choque con otros barcos (Colisiones).
-Rdo     	Devuelve una lista de tuplas [(0,1), (0,2)].	Devuelve el tablero de NumPy terminado.
+for c in propuestaCasillas: Recorre 1 a 1 las coords de c (fila y col) propuestas
+tablero_temp[c] == "-":     Mira en el tablero q hay en la coord c y comprueba si es agua ("-")
+all(...):                   devuelve True si todas las casillas de la lista cumplen la condición
+                            True = agua y se coloca el barco con "O". False= si 1 casilla ya ocupada por otro barco "O", 
+                            el programa rechaza la propuesta y busca otra ubicación aleatoria
+
+
+característica	crear_barco	                                    colocar_barcos_aleatorios
+Misión	        Crear coords de un barco                    	Gestionar la colocación de toda la flota
+VL      	    Q no se salga del tablero (Bordes)           	Q no choque con otros barcos (Colisiones)
+Rdo     	    Devuelve 1 lista de tuplas [(0,1), (0,2)]   	Devuelve el tablero terminado
 
 """
